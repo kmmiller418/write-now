885f78630bfc8388088cfb652625f0eb79b1a6d7
kmiller <kmmiller418@gmail.com>
2022-04-12 11:08:03 -0400

    Move form selectors into separate function, create start editor function

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const submit = document.querySelector("#submit");
 let time;
 let grace;
 let difficulty;
+let running;
 
 submit.addEventListener("click", (e) => {
   e.preventDefault();
+  setSettings();
+  startEditor();
+});
 
+const setSettings = () => {
   const radioButtons = document.querySelectorAll('input[name="difficulty"]');
   for (const radioButton of radioButtons) {
     if (radioButton.checked) {
@@ -20,4 +25,12 @@ submit.addEventListener("click", (e) => {
   console.log(difficulty);
   console.log(time);
   console.log(grace);
-});
+};
+
+const startEditor = () => {
+    const settings = document.querySelector('#settings-page');
+    const editor = document.querySelector('#writing-box');
+    settings.classList.add('hidden');
+    editor.classList.remove('hidden');
+}
+
